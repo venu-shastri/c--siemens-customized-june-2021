4bad868849fc2dc36dbffdd08aca2219a5545299
venu-shastri <venu.shastri@gmail.com>
2021-06-22 16:06:25 +0530

    Event Updated

--- WhyDelegate/ObservableProblem.cs
+++ WhyDelegate/ObservableProblem.cs
@@ -19,6 +19,7 @@ namespace WhyDelegate
         }
 
     }
+    //Observable- Notify
     public class Button
     {
         public void OnClick()
@@ -46,6 +47,73 @@ namespace WhyDelegate
                 Console.ReadKey();
                 _window.SimulateButtonClick();
             }
+        }
+    }
+
+    public enum SocketStatus
+    {
+        OPEN,CLOSED
+
+    }
+
+    //Subject
+    //Event Driven Programming 
+    public class Socket
+    {
+        SocketStatus status;
+        public event  Action StatusChanged;//Event
+        
+
+        ////Subscribe for Notification
+        //public void Add_StatusChanged(Action observer)
+        //{
+        //    this.StatusChanged += observer;
+        //}
+        ////UnSubscribe
+        //public void Remove_StatusChanged(Action observer)
+        //{
+        //    this.StatusChanged -= observer;
+        //}
+        public void Open() {
+            status = SocketStatus.OPEN;
+            this.NotifyAll();
+        }
+        public void Close() {
+            status = SocketStatus.CLOSED;
+            this.NotifyAll();
+        }
+        void NotifyAll()
+        {
+            if (this.StatusChanged != null)
+            {
+                //Raise An Event
+                this.StatusChanged.Invoke();//MulitiCast....
+            }
+        }
+    }
+
+    //Observable 
+    public class Application
+    {
+        Socket _socket = new Socket();
+        public Application()
+        {
+
+            //SubScube
+            //_socket.Add_StatusChanged(new Action(this.Update));
+            _socket.StatusChanged += new Action(this.Update);
+        }
+        void Update()
+        {
+            Console.WriteLine("Socket Status Changed");
+        }
+        public void Connect() {
+            _socket.Open();
+        }
+        public void DisConnect()
+        {
+            _socket.Close();
+
         }
     }
 }
