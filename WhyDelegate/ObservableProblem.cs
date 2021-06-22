cb366010fb2d0ad25e1ecc28c6e52865e5fc1903
venu-shastri <venu.shastri@gmail.com>
2021-06-22 15:13:14 +0530

    Create ObservableProblem.cs

---
+++ WhyDelegate/ObservableProblem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhyDelegate
+{
+    public class SearchWindow
+    {
+        Button _searchButton = new Button();
+        TextBox _textBox = new TextBox();
+
+        //Imitation Of UI Button Click
+        public void SimulateButtonClick()
+        {
+            _searchButton.OnClick();
+
+        }
+
+    }
+    public class Button
+    {
+        public void OnClick()
+        {
+
+        }
+
+    }
+    public class TextBox
+    {
+        public void Clear()
+        {
+            Console.WriteLine("Clearing TextBox Content !!!! .....Done")
+        }
+    }
+    public class ObservableProblem
+    {
+        static void Main()
+        {
+
+            SearchWindow _window = new SearchWindow();
+            while (true)
+            {
+                Console.WriteLine("Press Any Key To trigger Button Click");
+                Console.ReadKey();
+                _window.SimulateButtonClick();
+            }
+        }
+    }
+}
