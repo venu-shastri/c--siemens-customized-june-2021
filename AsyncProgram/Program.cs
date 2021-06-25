52c17203a72674d2cc30474b15eecaa81ea7e38f
venu-shastri <venu.shastri@gmail.com>
2021-06-25 11:36:09 +0530

    Threading Code
    Updated

--- AsyncProgram/Program.cs
+++ AsyncProgram/Program.cs
@@ -18,14 +18,22 @@ namespace AsyncProgram
             // Search("abc");//Synchrnous Program
             //begin New Thread(Execution Path) -> Search
            System.Threading.Thread _mainExecutionPath= System.Threading.Thread.CurrentThread;
-            Console.WriteLine($"Main Method Execution - Thread {System.Threading.Thread.CurrentThread.ManagedThreadId}");
-            System.Threading.Thread _searchExecutionPath = new System.Threading.Thread(
-                new System.Threading.ParameterizedThreadStart(SearchAsync));
-            _searchExecutionPath.Start(new System.Random().Next().ToString());
-            Console.WriteLine("End OF Main Execution Path");
+            Console.WriteLine($"Main Method Execution - Thread {System.Threading.Thread.CurrentThread.ManagedThreadId} Backgorund Mode ?{System.Threading.Thread.CurrentThread.IsBackground}");
+            //System.Threading.Thread _searchExecutionPath = new System.Threading.Thread(
+            //    new System.Threading.ParameterizedThreadStart(SearchAsync))
+            //{  IsBackground=true};
+            //_searchExecutionPath.Start(new System.Random().Next().ToString());
+
+            System.Threading.WaitCallback backGroundTaskFuncObj = new System.Threading.WaitCallback(SearchAsync);
+            System.Threading.ThreadPool.QueueUserWorkItem(backGroundTaskFuncObj, new System.Random().Next().ToString());
+
             
-           //string result= Search(new System.Random().Next().ToString());
+          //  _searchExecutionPath.Join();
+            Console.WriteLine("End OF Main Execution Path");
+
+            //string result= Search(new System.Random().Next().ToString());
             //Console.WriteLine($"Search Result {result} ");
+            Console.ReadLine();
            
 
         }
@@ -38,7 +46,7 @@ namespace AsyncProgram
         {
             for(int i = 0; i < 10; i++)
             {
-                Console.WriteLine($"Search Done  BY Thread: {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine($"Search Done  BY Thread: {System.Threading.Thread.CurrentThread.ManagedThreadId} Background Mode ?{System.Threading.Thread.CurrentThread.IsBackground}");
                 System.Threading.Thread.Sleep(1000);
             }
             return key;
---
+++ SychronizationApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SychronizationApp
+{
+    public class Singleton
+    {
+        public static readonly Singleton Instance = new Singleton();
+        private Singleton() { }
+
+        string state;
+
+        public void UpdateState() { 
+        for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Update State in Action ");
+                Thread.Sleep(1000);
+                
+            }
+        
+        }
+
+        public void AppendState() {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Append State in Action ");
+                Thread.Sleep(1000);
+
+            }
+        }
+        public void GetState() {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Get State in Action ");
+                Thread.Sleep(1000);
+
+            }
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Thread(Singleton.Instance.UpdateState).Start();
+            new Thread(Singleton.Instance.AppendState).Start();
+            new Thread(Singleton.Instance.GetState).Start(); 
+            
+        }
+    }
+}
