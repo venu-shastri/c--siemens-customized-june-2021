707401d0356d989c0b321df7fd17d1b73deb6ba2
venu-shastri <venu.shastri@gmail.com>
2021-06-25 13:48:59 +0530

    Monitor

--- SychronizationApp/Program.cs
+++ SychronizationApp/Program.cs
@@ -7,24 +7,51 @@ using System.Threading;
 
 namespace SychronizationApp
 {
-    public class Singleton
+    public class FileHandle
     {
-        public static readonly Singleton Instance = new Singleton();
+
+    }
+    //[System.Runtime.Remoting.Contexts.Synchronization]
+    public class Singleton //:System.ContextBoundObject
+    {
+        System.Object _syncUpdateAndAppend = new object();
+        System.Object _syncGetState = new object();
+
+        FileHandle _resource = new FileHandle();
+        public  static readonly Singleton Instance = null;
+        static Singleton()
+        {
+            Instance = new Singleton();
+        }
+        
+
+       
         private Singleton() { }
 
         string state;
+        private bool disposedValue;
 
-        public void UpdateState() { 
+        //T1
+        //[System.Runtime.CompilerServices.MethodImpl
+          //  (System.Runtime.CompilerServices.MethodImplOptions.Synchronized)]
+        public void UpdateState() {
+
+            Monitor.Enter(_syncUpdateAndAppend);
         for(int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Update State in Action ");
                 Thread.Sleep(1000);
                 
             }
+            Monitor.Exit(_syncUpdateAndAppend);
         
         }
 
+        //T2
+        //[System.Runtime.CompilerServices.MethodImpl
+          //  (System.Runtime.CompilerServices.MethodImplOptions.Synchronized)]
         public void AppendState() {
+            Monitor.Enter(_syncUpdateAndAppend);
 
             for (int i = 0; i < 10; i++)
             {
@@ -32,17 +59,25 @@ namespace SychronizationApp
                 Thread.Sleep(1000);
 
             }
+            Monitor.Exit(_syncUpdateAndAppend);
         }
+        //T3,T4
+        //[System.Runtime.CompilerServices.MethodImpl
+          //  (System.Runtime.CompilerServices.MethodImplOptions.Synchronized)]
         public void GetState() {
+            Monitor.Enter(_syncGetState);
+            //Critical Section 
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("Get State in Action ");
+                Console.WriteLine($"Get State in Action {System.Threading.Thread.CurrentThread.ManagedThreadId}");
                 Thread.Sleep(1000);
 
             }
+            Monitor.Exit(_syncGetState);
+
         }
 
-
+        
     }
     class Program
     {
@@ -50,8 +85,9 @@ namespace SychronizationApp
         {
             new Thread(Singleton.Instance.UpdateState).Start();
             new Thread(Singleton.Instance.AppendState).Start();
-            new Thread(Singleton.Instance.GetState).Start(); 
-            
+            new Thread(Singleton.Instance.GetState).Start();
+            new Thread(Singleton.Instance.GetState).Start();
+
         }
     }
 }
